ff52170b6d2d749ceff5b160c76789e9addbffff
Gabriel Horner <gabriel@logseq.com>
2022-06-23 11:35:02 -0400

    Try real composite action

--- dist/src/logseq/graph_parser/log.cljs
+++
@@ -1,6 +0,0 @@
-(ns logseq.graph-parser.log
-  "Minimal logging ns that implements basic lambdaisland.glogi fns. May use
-  glogi later if this ns is used more")
-
-(defn error [& msgs]
-  (apply js/console.error (map clj->js msgs)))
